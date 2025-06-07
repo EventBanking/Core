175d022a335dea562c5fc49a6ce28f579a3d45af
Daniel Aguirre <uhyeay24@gmail.com>
2025-06-07 16:21:09 -0500

    added LogMessage

---
+++ EventBankingCo.Core.Domain/Models/LogMessage.cs
@@ -0,0 +1,31 @@
+﻿namespace EventBankingCo.Core.Domain.Models
+{
+    public class LogMessage
+    {
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string Level { get; set; } = string.Empty;
+
+        public string? Message { get; set; } = string.Empty;
+
+        public string? MessageTemplate { get; set; }
+
+        public string? Properties { get; set; }
+
+        public string? TraceId { get; set; }
+
+        public string? ClassName { get; set; }
+
+        public string? MethodName { get; set; }
+
+        public string? CorrelationId { get; set; }
+
+        public string? ExceptionType { get; set; }
+
+        public string? ExceptionMessage { get; set; }
+
+        public string? StackTrace { get; set; }
+    }
+}
