0b1fac5da021ba994700bde6da5117f25f28b7b6
Daniel Aguirre <uhyeay24@gmail.com>
2025-06-20 22:24:20 -0500

    set up base DataRequestHandler and DataCommandHandler for requests that will integrate w/ database via IDataAccess

---
+++ EventBankingCo.Core.ApiShared/Base/DataCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using EventBankingCo.Core.DataAccess.Abstraction;
+using EventBankingCo.Core.Logging.Abstraction;
+using EventBankingCo.Core.RequestHandling.Abstraction;
+using EventBankingCo.Core.RequestHandling.Base;
+
+namespace EventBankingCo.Core.ApiShared.Base
+{
+    public abstract class DataCommandHandler<TRequest> : CommandHandler<TRequest> where TRequest : ICommand
+    {
+        protected readonly IDataAccess _dataAccess;
+
+        protected DataCommandHandler(ICoreLoggerFactory loggerFactory, IDataAccess dataAccess) : base(loggerFactory)
+        {
+            _dataAccess = dataAccess;
+        }
+    }
+}
---
+++ EventBankingCo.Core.ApiShared/Base/DataRequestHandler.cs
@@ -0,0 +1,17 @@
+﻿using EventBankingCo.Core.DataAccess.Abstraction;
+using EventBankingCo.Core.Logging.Abstraction;
+using EventBankingCo.Core.RequestHandling.Abstraction;
+using EventBankingCo.Core.RequestHandling.Base;
+
+namespace EventBankingCo.Core.ApiShared.Base
+{
+    public abstract class DataRequestHandler<TRequest, TResponse> : RequestHandler<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        protected readonly IDataAccess _dataAccess;
+
+        protected DataRequestHandler(ICoreLoggerFactory loggerFactory, IDataAccess dataAccess) : base(loggerFactory)
+        {
+            _dataAccess = dataAccess;
+        }
+    }
+}
