2df54c026fade0366bdc3be8e2b4875407c8033e
Daniel Aguirre <uhyeay24@gmail.com>
2025-06-20 22:25:20 -0500

    added constants for SqlExceptionNumber

---
+++ EventBankingCo.Core.Domain/Exceptions/SqlExceptionNumber.cs
@@ -0,0 +1,9 @@
+﻿namespace EventBankingCo.Core.Domain.Exceptions
+{
+    public static class SqlExceptionNumber
+    {
+        public const int UniqueConstraintViolation = 2627;
+
+        public const int ForeignKeyViolation = 547;
+    }
+}
