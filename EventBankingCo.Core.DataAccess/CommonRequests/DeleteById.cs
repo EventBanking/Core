514cf533894a8b8435b3838d5c4ff6f4dcd57e3a
Daniel Aguirre <uhyeay24@gmail.com>
2025-06-20 22:25:37 -0500

    added base classes for DeleteById and FetchById DataRequests

---
+++ EventBankingCo.Core.DataAccess/CommonRequests/DeleteById.cs
@@ -0,0 +1,20 @@
+﻿using EventBankingCo.Core.DataAccess.DataRequests;
+
+namespace EventBankingCo.Core.DataAccess.CommonRequests
+{
+    public abstract class DeleteById : SqlCommand
+    {
+        protected DeleteById(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+
+        public abstract string GetTableName();
+
+        public override object? GetParameters() => new { Id };
+
+        public override string GetSql() => $"DELETE FROM {GetTableName()} WHERE Id = @Id;";
+    }
+}
---
+++ EventBankingCo.Core.DataAccess/CommonRequests/FetchById.cs
@@ -0,0 +1,22 @@
+﻿using EventBankingCo.Core.DataAccess.DataRequests;
+
+namespace EventBankingCo.Core.DataAccess.CommonRequests
+{
+    public abstract class FetchById<T> : SqlFetch<T>
+    {
+        public int Id { get; set; }
+
+        public FetchById(int id)
+        {
+            Id = id;
+        }
+
+        public abstract string GetTableName();
+
+        public virtual string GetColmuns() => "*";
+
+        public override object? GetParameters() => new { Id };
+
+        public override string GetSql() => $"SELECT {GetColmuns()} FROM {GetTableName()} WITH(NOLOCK) WHERE Id = @Id;";
+    }
+}
